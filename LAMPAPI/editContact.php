51dd46f375e5cb000d46b863f9bd72aa9419381a
Jonathan Velez <jonathan14982@gmail.com>
2022-09-16 14:00:17 -0400

    add phone number and email to search function

--- LAMPAPI/editContact.php
+++ LAMPAPI/editContact.php
@@ -24,13 +24,13 @@
 	}
 	else
 	{
-		//Search for a contact owned by the user with the same name
-		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE Name LIKE ?");
-		$stmt->bind_param("s", $name);
+		//Search for a contact owned by the user with the same name, phone number, and email
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE Name LIKE ? AND Phone_number=? AND Email=?");
+		$stmt->bind_param("sss", $name, $phoneNumber, $email);
 		$stmt->execute();
 		$result = $stmt->get_result();
 		
-		//Process results into a string
+		//Process results into a string and counts the number of entries that are the same
 		while($row = $result->fetch_assoc())
 		{
 			if( $searchCount > 0 )
@@ -41,13 +41,12 @@
 			$searchResults .= '{' . '"name": ' . '"' . $row["Name"] . '",';
 			$searchResults .= '"phoneNumber": ' . '"' . $row["Phone_number"] . '",';
 			$searchResults .= '"email": ' . '"' . $row["Email"] . '",';
-			$searchResults .= '"id": ' . $row["ID"] . '}';
 
 		}
 
 		
 		//Check if a duplicate contact exists
-		if( $searchCount != 1 ) 
+		if( $searchCount != 1 && $searchCount != 0) 
 		{
 			//A duplicate entry was found in the users contacts
 			returnWithError( 0, "A duplicate contact exists, enter a diffrent name");
